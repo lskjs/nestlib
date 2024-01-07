bae93a32f57284261f8e944b5b96a9fca6bc7128
Igor Suvorov <hi@isuvorov.com>
2024-01-07 19:28:05 +0300

    feat(mikro-orm): improve query logger

--- libs/mikro-orm/src/loggerFactory.ts
+++ libs/mikro-orm/src/loggerFactory.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-nested-ternary */
 import type { ILogger } from '@lsk4/log';
 import { createLogger } from '@lsk4/log';
+// @ts-ignore
+import { prettyTime } from '@lsk4/log/pretty';
 import { LogContext, Logger, LoggerNamespace, LoggerOptions } from '@mikro-orm/core';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -11,7 +14,21 @@ export const loggerFactory = (options: LoggerOptions): Logger => {
   };
   return {
     logQuery(context: LogContext) {
-      getLogger('query').trace(...[context.query, context.params].filter(Boolean));
+      const { level: initLevel, query, params, took, results } = context;
+      let level: 'trace' | 'debug' | 'warn' | 'error' | undefined;
+      if (initLevel === 'warning') level = 'warn';
+      if (initLevel === 'error') level = 'error';
+      if (!level) {
+        level = took! > 60000 ? 'warn' : took! > 5000 ? 'debug' : 'trace';
+      }
+      let info = '';
+      info += took ? prettyTime(took) : '';
+      if (results! > 1) info += ` [${results}]`;
+      const prettyQuery = String(query)
+        .replace(/^db.getCollection\('(\w+)'\)/g, '$1')
+        .replace(/.limit\(1\).toArray\(\);$/g, '')
+        .replace(/, \{\}\)$/g, ')');
+      getLogger('query')[level](...[prettyQuery, params, info].filter(Boolean));
     },
     log(ns: LoggerNamespace, message: string) {
       getLogger(ns).debug(message);
