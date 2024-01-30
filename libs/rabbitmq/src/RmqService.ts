d080576ff4c5f93a386e7aa063fe06f73cacd3b1
Igor Suvorov <hi@isuvorov.com>
2024-01-30 12:30:04 +0300

    fix(rabbitmq): fix rmq type

--- libs/rabbitmq/src/RmqService.ts
+++ libs/rabbitmq/src/RmqService.ts
@@ -9,14 +9,15 @@ export class RmqService {
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
   async publish(exchange: string, routingKey: string, payload: RmqRequestPayload) {
+    const { pattern, ...other } = payload || {};
     const ns = ['rmq:publish', exchange, routingKey].filter(Boolean).join(':');
     // TODO: add cache
     const log = createLogger(ns);
-    log.trace(payload);
+    log.trace(`[${pattern}]`, other);
     const time = Date.now();
     const res = await this.amqpConnection.publish(exchange, routingKey, payload);
     const ms = Date.now() - time;
-    log.debug(payload, res || 'OK', `${ms}ms`);
+    log.debug(`[${pattern}]`, other, res || 'OK', `${ms}ms`);
     return res;
   }
   async request(raw: {
@@ -25,15 +26,16 @@ export class RmqService {
     timeout: number;
     payload: RmqRequestPayload;
   }) {
-    const { exchange, routingKey, payload } = raw;
+    const { exchange, routingKey, payload } = raw || {};
+    const { pattern, ...other } = payload || {};
     const ns = ['rmq:request', exchange, routingKey].filter(Boolean).join(':');
     // TODO: add cache
     const log = createLogger(ns);
-    log.trace(payload);
+    log.trace(`[${pattern}]`, other);
     const time = Date.now();
     const res = await this.amqpConnection.request(raw);
     const ms = Date.now() - time;
-    log.debug(payload, res || 'OK', `${ms}ms`);
+    log.debug(`[${pattern}]`, other, res || 'OK', `${ms}ms`);
     return res;
   }
 }
