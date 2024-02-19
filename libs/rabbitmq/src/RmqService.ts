034718033450f9caec5e47790e486733e5c8cd91
Igor Suvorov <hi@isuvorov.com>
2024-02-19 19:16:16 +0300

    feat(rabbitmq): add publish options

--- libs/rabbitmq/src/RmqService.ts
+++ libs/rabbitmq/src/RmqService.ts
@@ -1,6 +1,7 @@
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { createLogger } from '@lsk4/log';
 import { Injectable } from '@nestjs/common';
+import { Options } from 'amqplib';
 
 import { RmqRequestPayload } from './types';
 
@@ -8,14 +9,19 @@ import { RmqRequestPayload } from './types';
 export class RmqService {
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
-  async publish(exchange: string, routingKey: string, payload: RmqRequestPayload) {
+  async publish(
+    exchange: string,
+    routingKey: string,
+    payload: RmqRequestPayload,
+    options?: Options.Publish,
+  ) {
     const { pattern, ...other } = payload || {};
     const ns = ['rmq:publish', exchange, routingKey].filter(Boolean).join(':');
     // TODO: add cache
     const log = createLogger(ns);
     log.trace(`[${pattern}]`, other);
     const time = Date.now();
-    const res = await this.amqpConnection.publish(exchange, routingKey, payload);
+    const res = await this.amqpConnection.publish(exchange, routingKey, payload, options);
     const ms = Date.now() - time;
     log.debug(`[${pattern}]`, other, res || 'OK', `${ms}ms`);
     return res;
