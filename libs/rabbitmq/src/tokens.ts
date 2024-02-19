c1267b73adeee8ce73905d2c75fe6ece241f77d7
Igor Suvorov <hi@isuvorov.com>
2024-02-19 14:00:33 +0300

    fix(rabbitmq): rename tokens

--- libs/rabbitmq/src/tokens.ts
+++ libs/rabbitmq/src/tokens.ts
@@ -1,6 +1,5 @@
-export const NESTLIB_RMQ_CONNECTION = 'default';
-export const NESTLIB_RMQ_SERVICE_TOKEN = 'NestlibRmqServiceToken';
+export const NESTLIB_RMQ_SERVICE_TOKEN = 'nestlib:RmqService';
 
-export function getRmqServiceToken(connection?: string): string {
-  return `${connection || NESTLIB_RMQ_CONNECTION}_${NESTLIB_RMQ_SERVICE_TOKEN}`;
+export function getRmqServiceToken(ns?: string): string {
+  return [ns, NESTLIB_RMQ_SERVICE_TOKEN].filter(Boolean).join(':');
 }
