10dfba99779c4278e76eadef84c82eb9ffe045e4
Igor Suvorov <hi@isuvorov.com>
2024-05-29 17:10:52 +0300

    feat(utils): add AnyValidataionPipe

---
+++ libs/utils/src/AnyValidataionPipe.ts
@@ -0,0 +1,30 @@
+import { Err } from '@lsk4/err';
+import { ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
+import type { ValidationError } from 'class-validator';
+
+export class AnyValidataionPipe extends ValidationPipe {
+  constructor(options?: ValidationPipeOptions) {
+    super({
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        const messages = validationErrors.map((error: any) => {
+          let message;
+          const keys = Object.keys(error.constraints);
+          if (keys.length > 1) {
+            message = Object.values(error.constraints);
+          } else {
+            message = error.constraints[keys[0]];
+          }
+          return {
+            property: error.property,
+            message,
+          };
+        });
+        return new Err('Validation Error', {
+          status: 400,
+          data: messages,
+        });
+      },
+      ...options,
+    });
+  }
+}
