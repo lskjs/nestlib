ad1535004f2c976627a759077230319da6c60d36
Igor Suvorov <hi@isuvorov.com>
2024-05-30 16:56:58 +0300

    feat(notify): add forRootAsync

--- libs/notify/src/Notify.module.ts
+++ libs/notify/src/Notify.module.ts
@@ -1,8 +1,14 @@
-import { DynamicModule, Module } from '@nestjs/common';
+/* eslint-disable no-return-await */
+import { Err } from '@lsk4/err';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 
 import { NotifyService } from './Notify.service.js';
-import { NOTIFY_MODULE_OPTIONS_TOKEN } from './tokens.js';
-import type { NotifyModuleOptions } from './types.js';
+import { getNotifyServiceToken, NOTIFY_MODULE_OPTIONS_TOKEN } from './tokens.js';
+import type {
+  NotifyModuleAsyncOptions,
+  NotifyModuleOptions,
+  NotifyModuleOptionsFactory,
+} from './types.js';
 
 @Module({
   providers: [NotifyService],
@@ -10,6 +16,11 @@ import type { NotifyModuleOptions } from './types.js';
 })
 export class NotifyModule {
   static forRoot(options: NotifyModuleOptions): DynamicModule {
+    const NotifyServiceByToken = {
+      // TODO: think how to pass namespace?
+      provide: getNotifyServiceToken(options.namespace),
+      useExisting: NotifyService,
+    };
     return {
       module: NotifyModule,
       providers: [
@@ -18,8 +29,61 @@ export class NotifyModule {
           useValue: options,
         },
         NotifyService,
+        NotifyServiceByToken,
       ],
-      exports: [NotifyService],
+      exports: [NotifyService, NotifyServiceByToken],
+    };
+  }
+
+  private static createAsyncProviders(options: NotifyModuleAsyncOptions): Provider[] {
+    if (options.useFactory) {
+      return [
+        {
+          provide: NOTIFY_MODULE_OPTIONS_TOKEN,
+          useFactory: options.useFactory,
+          inject: options.inject || [],
+        },
+      ];
+    }
+    if (options.useClass) {
+      return [
+        {
+          provide: NOTIFY_MODULE_OPTIONS_TOKEN,
+          useFactory: async (optionsFactory: NotifyModuleOptionsFactory) =>
+            await optionsFactory.createNotifyModuleOptions(),
+          inject: [options.useClass],
+        },
+        {
+          provide: options.useClass,
+          useClass: options.useClass,
+        },
+      ];
+    }
+    if (options.useExisting) {
+      return [
+        {
+          provide: NOTIFY_MODULE_OPTIONS_TOKEN,
+          useFactory: async (optionsFactory: NotifyModuleOptionsFactory) =>
+            await optionsFactory.createNotifyModuleOptions(),
+          inject: [options.useExisting],
+        },
+      ];
+    }
+    throw new Err('Invalid configuration. Must provide useFactory, useClass, or useExisting.');
+  }
+
+  static forRootAsync(options: NotifyModuleAsyncOptions): DynamicModule {
+    const asyncProviders = this.createAsyncProviders(options);
+    const NotifyServiceByToken = {
+      provide: getNotifyServiceToken(options.namespace),
+      useExisting: NotifyService,
+    };
+
+    return {
+      module: NotifyModule,
+      imports: options.imports || [],
+      providers: [...asyncProviders, NotifyService, NotifyServiceByToken],
+      exports: [NotifyService, NotifyServiceByToken],
     };
   }
 }
--- libs/notify/src/types.ts
+++ libs/notify/src/types.ts
@@ -1,5 +1,21 @@
 import type { RlogOptions } from '@lsk4/rlog';
+import { Type } from '@nestjs/common';
 
 export type NotifyModuleOptions = RlogOptions & {
+  namespace: string;
   // some custom options
 };
+
+export interface NotifyModuleOptionsFactory {
+  createNotifyModuleOptions(): Promise<NotifyModuleOptions> | NotifyModuleOptions;
+}
+
+export interface NotifyModuleAsyncOptions {
+  namespace: string;
+
+  useExisting?: Type<NotifyModuleOptionsFactory>;
+  useClass?: Type<NotifyModuleOptionsFactory>;
+  useFactory?: (...args: any[]) => Promise<NotifyModuleOptions> | NotifyModuleOptions;
+  inject?: any[];
+  imports?: any[];
+}
