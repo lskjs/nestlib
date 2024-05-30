88cbb019af349264fe7e9db32b4013d58e033f09
Igor Suvorov <hi@isuvorov.com>
2024-05-30 12:21:09 +0300

    no message

--- libs/interceptors/src/UseAnyInterceptor.ts
+++ libs/interceptors/src/UseAnyInterceptor.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { applyDecorators, NestInterceptor, UseInterceptors } from '@nestjs/common';
-import { ErrorInterceptor, ResponseInterceptor } from '@nestlib/interceptors';
+
+import { ErrorInterceptor } from './ErrorInterceptor.js';
+import { ResponseInterceptor } from './ResponseInterceptor.js';
 
 export function UseAnyInterceptor(...interceptors: (Function | NestInterceptor<any, any>)[]) {
   return applyDecorators(UseInterceptors(ResponseInterceptor, ErrorInterceptor, ...interceptors));
