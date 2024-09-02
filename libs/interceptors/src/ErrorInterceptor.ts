30b3a103db6907621d251a9c2a9373c8312881e1
Igor Suvorov <hi@isuvorov.com>
2024-09-02 09:44:39 +0300

    feat(interceptors): add debug levels for error intercepter

--- libs/interceptors/src/ErrorInterceptor.ts
+++ libs/interceptors/src/ErrorInterceptor.ts
@@ -1,11 +1,12 @@
 import { omitBy } from '@lsk4/algos';
-import { isDev } from '@lsk4/env';
+import { isDev, stage } from '@lsk4/env';
 import { Err } from '@lsk4/err';
 import {
   CallHandler,
   ExecutionContext,
   HttpException,
   Injectable,
+  InternalServerErrorException,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
@@ -19,18 +20,48 @@ export class ErrorInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((error = {}) => {
-        // TODO: get from config
-        // TODO: подумать о уровнях логирования ошибок
-        // 1. локальная разработка, писать в debug всю внутрянку, чтобы на фронте и в консоли видеть причину
-        // 2. дев сервер -- пусть (опционально включать) тоже самое внутрянку, когда нужно. (подумать по статусы, например внутрянку 400х ошибок и на локалке порой не нужно)
-        // 3. бета сервер -- писать в логи debug, но не отправлять пользователю. отправлять пользователю только code, message
-        // 4. продакшн -- отправлять пользователю только определенные code, message, всё что не в whitelist - заменять на общее сообщение
-        // TODO: так же подумать о разных статусах, например для прод сервера не нужны подробности 400х ошибок в консоли, но нужно подробности 500х
-        const isDebug = isDev;
-
         // eslint-disable-next-line prefer-const
         let [code, message] = Err.getCodeMessage(error);
-        const { status, err, data, ...rawDebug } = error || {};
+        const {
+          status: rawStatus,
+          // statusCode: rawStatusCode,
+          err,
+          data,
+          ...rawDebug
+        } = error || {};
+        const status =
+          rawStatus ||
+          // TODO: подумать можно ли так кидаться?
+          // rawStatusCode ||
+          500;
+
+        // TODO: подумать о уровнях логирования ошибок
+        // 3. локальная разработка, писать в debug всю внутрянку, чтобы на фронте и в консоли видеть причину
+        // 2. дев сервер -- пусть (опционально включать) тоже самое внутрянку, когда нужно. (подумать по статусы, например внутрянку 400х ошибок и на локалке порой не нужно)
+        // 1. бета сервер -- писать в логи debug, но не отправлять пользователю. отправлять пользователю только code, message
+        // 0. продакшн -- отправлять пользователю только определенные code, message, всё что не в whitelist - заменять на общее сообщение
+        // TODO: так же подумать о разных статусах, например для прод сервера не нужны подробности 400х ошибок в консоли, но нужно подробности 500х
+
+        // TODO: get from config
+        // eslint-disable-next-line no-nested-ternary
+        const debugLevel = isDev ? 3 : stage === 'dev' ? 2 : stage === 'beta' ? 1 : 0;
+        // const debugLevel = 2;
+        const isShowDebug = debugLevel >= 3;
+        const isInternalError = status >= 500;
+        const isShowConsole = isShowDebug || isInternalError;
+        const isShowInternalError = debugLevel >= 2;
+        const isShowError = isShowDebug || (isInternalError ? isShowInternalError : true);
+        // console.log({
+        //   debugLevel,
+        //   isShowDebug,
+        //   isInternalError,
+        //   isShowConsole,
+        //   isShowError,
+        //   rawStatus,
+        // });
+        // const isInteral = debugLevel >= 2;
+        // const isDebug = isDev;
+
         const debug = omitBy(
           rawDebug,
           (v, k) =>
@@ -38,14 +69,32 @@ export class ErrorInterceptor implements NestInterceptor {
             v == null ||
             (k === 'name' && v === 'Err'),
         );
-        const { name } = debug;
 
-        // NOTE: может не надо делать setTimeout?
-        setTimeout(() => {
-          printPrettyError(error);
-        }, 0);
+        if (isShowConsole) {
+          // NOTE: может не надо делать setTimeout?
+          setTimeout(() => {
+            printPrettyError(error);
+          }, 0);
+        }
 
         context.switchToHttp().getRequest().nestError = error;
+
+        if (!isShowError) {
+          return throwError(
+            () =>
+              new InternalServerErrorException(
+                {
+                  ok: 0,
+                  code: 'errInternal',
+                  message: 'Internal Server Error',
+                },
+                {
+                  cause: error,
+                  description: message,
+                },
+              ),
+          );
+        }
 
         const res = {} as any;
         if (!message) {
@@ -56,12 +105,10 @@ export class ErrorInterceptor implements NestInterceptor {
           //   res.message = 'The Unknown Error';
           // }
         }
-        if (!status || !(status >= 400 && status <= 600)) {
-          res.status = 500;
-        }
-        if (!code) res.code = name || `err${res.status || 500}`;
+        if (!code) res.code = `err${status}`;
         if (code === 'Error') code = 'errUnknown';
-        if (isDebug && debug && Object.keys(debug).length) {
+        if (isInternalError) code = `errInternal(${code})`;
+        if (isShowDebug && debug && Object.keys(debug).length) {
           res.debug = debug;
         }
         if (err) {
@@ -83,9 +130,8 @@ export class ErrorInterceptor implements NestInterceptor {
           { ok: 0, code, message, status, ...res },
           { toString: false },
         );
-        const status2 = status || res.status;
         const options = { cause: error, description: message };
-        return throwError(() => new HttpException(packedData, status2, options));
+        return throwError(() => new HttpException(packedData, status, options));
       }),
     );
   }
